f4cede9e4a3b3d407a0a23fa013ed95c06d701f9
Kateřina Petrášková <ka.petraskova@gmail.com>
2022-05-18 17:22:50 +0200

    Oprava opravy :)

--- src/main/java/cz/czechitas/ukol4/Sportka.java
+++ src/main/java/cz/czechitas/ukol4/Sportka.java
@@ -11,7 +11,7 @@ public class Sportka {
   private final List<Integer> osudi = new ArrayList<>();
 
   public Sportka() {
-    for (int i = 1; i < 49; i++) {
+    for (int i = 1; i < 50; i++) {
       osudi.add(i);
     }
   }
